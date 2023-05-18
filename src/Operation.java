eb3cb34a8d95739675617357bc749d3da86fff3b
MURAT <muratdura03@gmail.com>
2023-05-18 22:30:48 +0300

    Beginning

---
+++ src/Operation.java
@@ -0,0 +1,30 @@
+public class Operation implements Comparable<Operation> {
+    int burst_time;
+    String arrival_date;
+    int arrival_time;
+    String task_type;
+    int priority;
+    Operation next;
+
+    public Operation(int data, int priority) {
+        this.burst_time = burst_time;
+        this.priority = priority;
+        this.arrival_date = arrival_date;
+        this.task_type = task_type;
+        this.next = null;
+    }
+
+    public int compareTo(Operation other) {
+        if (priority < other.priority) {
+            return -1;
+        } else if (priority > other.priority) {
+            return 1;
+        } else if (arrival_time < other.arrival_time) {
+            return -1;
+        } else if (arrival_time > other.arrival_time) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/PriorityQueue.java
@@ -0,0 +1,9 @@
+public interface PriorityQueue <T extends Comparable<? super T>>{
+    public void add(T newEntry);
+    public T remove();
+    public T peek();
+    public boolean isEmpty();
+    public int getSize();
+    public void clear();
+
+}
